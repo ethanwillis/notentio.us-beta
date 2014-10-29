87590fc989065123a1c3eafb03584ad8a33cd58c
Ethan Willis <ewillis1@memphis.edu>
2014-10-28 20:09:00 -0500

    Updated the alert for successful signup to be more aesthetically pleasing.

--- client/views/index.js
+++ client/views/index.js
@@ -4,6 +4,16 @@ Template.signup_counter.rendered = function() {
 	$('#signupCounter').flipcountdown({ size: 'lg', tick: 1000});
 }
 
+Template.index.rendered = function() {
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-56146416-1', 'auto');
+  ga('send', 'pageview');
+}
+
 Template.signup_form.events({
 	'click #signupBtn': function(evnt, tmpl) {
 		var email = emailAddress.value;
